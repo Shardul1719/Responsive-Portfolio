a87df947a81212bd766034f8d3542ccaa0c992ae
Shardul1719 <shardulj26@gmail.com>
2025-08-21 01:15:02 +0530

    updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -74,7 +74,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/Shardul_Jadhav.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
